569e680b71a33ab2b2c256d330b4d41e3d61dfaf
surbina <sheyla.ulopez@gmail.com>
2024-05-13 18:25:19 -0600

    initial commit

---
+++ src/test/java/com/git/property/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
